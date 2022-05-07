739fd0bbc5b9fb14593055601de3d7ef4bb0ca32
Owuor Ian <71072271+owuorian@users.noreply.github.com>
2022-05-07 17:36:38 +0300

    Add files via upload

---
+++ problem9.py
@@ -0,0 +1,18 @@
+import math
+from pickle import BINFLOAT
+
+c = 0
+found = False
+for a in range(1, 999):
+    for b in range(1, 999):
+        c = 1000 - a - b
+
+        if math.sqrt(a**2 + b**2) == c:
+            found = True
+            break
+
+    if found:
+        break
+
+print(a, b, c)
+print(a * b * c)
